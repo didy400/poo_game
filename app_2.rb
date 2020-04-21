18fb7fc105fcd794f1210a5e1f4cee097f6c272a
didy400 <didy.and@gmail.com>
2020-04-21 20:04:49 +0200

    app_2 incusion

---
+++ app_2.rb
@@ -0,0 +1,88 @@
+require 'pry'
+require 'bundler'
+Bundler.require
+
+require_relative 'lib/game'
+require_relative 'lib/player'
+
+
+puts "WELLCOME to battle for KAAMELOTT"
+
+puts "Qui est encore ce bras cassé..?"
+print "(choisis le nom du chevalier de la table ronde c'est plus drôle ;) )\n
+------>"
+human1 = HumanPlayer.new(gets.chomp)
+
+
+player1 = Player.new("Ivain le chevalier au lion")
+player2 = Player.new("Govain le petit pedestre dont un au lion")
+players = [player1,player2]
+
+while human1.life_points > 0 || (player1.life_points > 0 && player2 > 0)
+    human1.show_state
+    puts "Que veux tu faire?"
+    puts "\na - chercher un moyen pour mieux pouvoir attaquer ces monstreux Lapins Adultes"
+    puts "s - te cacher derrière une plque d'invisibilité et voir si tu peux améliorer ta situation"
+    puts "\n partir à l'assaut d'un chevalier de la table ronde"
+    if player1.life_points > 0
+        player1.show_state
+        puts "0 - Ivain"
+    end
+     if player2.life_points > 0
+        player2.show_state
+        puts "1 - Gauvain"   
+    end
+    print "--->"
+    choice = gets.chomp
+    if choice == "b"
+        human1.search_health_pack
+    elsif choice == "a"
+        human1.seach_weapon
+    elsif choice == 0
+        human1.attacks(player1)
+    elsif choice == 1
+        human1.attacks(player2)
+    else
+        "Encore cette chançon qui reste....\n
+        \nMon petit oiseau\n
+        A pris sa volée\n
+        Mon petit oiseau\n
+        A pris sa volée\n
+        A pris sa, à la volette\n
+        A pris sa, à la volette\n
+        A pris sa volée\n
+        
+        \nEst allé se mettre\n
+        Sur un oranger\n
+        Est allé se mettre\n
+        Sur un oranger\n
+        Sur un o, à la volette\n
+        Sur un o, à la volette\n
+        Sur un oranger\n
+        
+        \nLa branche était sèche\n
+        L'oiseau est tombé\n
+        La branche était sèche\n
+        L'oiseau est tombé\n
+        L'oiseau est, à la volette\n
+        L'oiseau est, à la volette\n
+        L'oiseau est tombé\n"
+    end
+
+    players.each do |i|
+        if i.life_points > 0
+            i.attacks(human1) 
+        end
+    end
+end
+
+if human1.life_points > 0
+    puts "Te voila courroné roi de Bretagne"
+else
+    puts "Les petits pédestres dont un au lion ont eu raison de toi!"
+end
+=begin
+puts "voici l'état de la situation!"
+puts player1.show_state
+puts player2.show_state
+=end
--- lib/player.rb
+++ lib/player.rb
@@ -48,3 +48,58 @@ end
      # def self.find_by_name(name)
     #    @@players.include?(name) ? @@players[name] : false
     #end
+
+class HumanPlayer < Player
+    attr_accessor :weapon_level
+
+    def initialize(name)
+        @name = name
+        life_points = 100
+        @life_points = life_points
+        weapon_level = 1
+        @weapon_level = weapon_level
+    end
+
+    def show_state 
+        puts "#{@name} à #{@life_points}pv et une pierre incandéscante de niveau #{@weapon_level}"
+    end
+
+    def compute_damage
+        damage = rand(0..6)* @weapon_level
+        return damage
+    end
+
+    def seach_weapon
+        new_weapon = rand(1..6)
+        puts "Tu as trouvé une pierre incandéscante de niveau #{new_weapon}"
+        if new_weapon > @weapon_level
+            @weapon_level = new_weapon
+            if new_weapon == 6
+               puts "He mais elle disait pas des conneries la vieille! le graal est une pièrre incandéscante!"
+            end
+            puts"Elle brille de milles feux!"
+        else
+            puts "J'ai mis mon doigt dans le cul du coq pour savoir le sens du vent... He dis donc il en dirait pas un peut trop souvent des conneries vot'Kadok?"
+
+        end
+    end
+    
+    def search_health_pack
+        potion = rand(1..6)
+        if potion == 1
+            puts "j'ai frollé le sol de façon péramptoir!"
+        elsif potion > 1 && potion < 6
+            puts "Camouflage éducloré!"
+            @life_points += 50
+            puts "50 PV en plus wooohooooo!!!!"
+        elsif potion > 5
+            puts "Oh putain il est fort ce con!"
+            @life_points += 80
+            puts "Je me sent comme larron en foire! et de 80 PV!!"
+        end
+        if @life_points > 100
+            @life_points = 100
+            puts "Hep hep hep! Pas plus de 100 PV! \n Ouais c'est pas faux!"
+        end
+    end
+end
