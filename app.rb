2922042345dc6867555ba11a545e59dc4c94043d
didy400 <didy.and@gmail.com>
2020-04-21 17:46:45 +0200

    second commit

--- app.rb
+++ app.rb
@@ -8,4 +8,19 @@ require_relative 'lib/player'
 player1 = Player.new("Guenièvre à la blanche fesse")
 player2 = Player.new("Dame Mevanwi dit la truie")
 
-binding.pry 
+puts "voici l'état de la situation!"
+puts player1.show_state
+puts player2.show_state
+
+while player1.life_points > 0 && player2.life_points > 0
+
+    puts "\n Les prétendantes au rône de bretagne vont maintenant s'affrotter pour 
+    detterminer laquelle des deux mérrite le coeur de nottre bon roi.\n"  
+
+    player1.attacks(player2)
+    puts "\n"
+   if player2.life_points > 0
+     player2.attacks(player1)
+   end
+end
+
